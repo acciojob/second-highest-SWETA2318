0bd6d80e2fde61836ed612bdceeea4ffa9ffa70f
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-26 19:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length <= 1 || new Set(arr).size === 1) {
+        return -Infinity;
+    }
+
+    // Remove duplicates and sort the array in descending order
+    const uniqueSortedArr = Array.from(new Set(arr)).sort((a, b) => b - a);
+
+    // Return the second element in the sorted array
+    return uniqueSortedArr[1];
+}
+
+// Examples
+console.log(secondHighest([5, 1, 2, 3, 4]));   // Output: 4
+console.log(secondHighest([-1, -2, -3, -4, -5]));  // Output: -2
+console.log(secondHighest([]));   // Output: -Infinity
+console.log(secondHighest([1]));   // Output: -Infinity
+console.log(secondHighest([1, 1, 1, 1, 1]));   // Output: -Infinity
