13ab30ba9451ccb3d0b79a44b9d973462b829b02
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-26 19:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,24 +3,18 @@ function secondHighest(arr) {
         return -Infinity;
     }
 
-    // Find the maximum element
-    const max = Math.max(...arr);
+    let firstMax = -Infinity;
+    let secondMax = -Infinity;
 
-    // Find the index of the maximum element
-    const maxIndex = arr.indexOf(max);
-
-    // Remove the maximum element from the array
-    arr.splice(maxIndex, 1);
-
-    // Find the second maximum element
-    const secondMax = Math.max(...arr);
+    for (let num of arr) {
+        if (num > firstMax) {
+            secondMax = firstMax;
+            firstMax = num;
+        } else if (num > secondMax && num < firstMax) {
+            secondMax = num;
+        }
+    }
 
     return secondMax;
 }
 
-// Examples
-console.log(secondHighest([5, 1, 2, 3, 4]));   // Output: 4
-console.log(secondHighest([-1, -2, -3, -4, -5]));  // Output: -2
-console.log(secondHighest([]));   // Output: -Infinity
-console.log(secondHighest([1]));   // Output: -Infinity
-console.log(secondHighest([1, 1, 1, 1, 1]));   // Output: -Infinity
