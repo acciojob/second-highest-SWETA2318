42bee9244d0b069219f6b2af18f4082f2bb56960
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2023-12-27 11:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function secondHighest(arr) {
-    if (arr.length < 2 || new Set(arr).size === 1) {
-        return -Infinity;
+  if (arr.length < 2 || new Set(arr).size === 1) {
+    return -Infinity;
+  }
+
+  let firstMax = Math.max(...arr);
+  let secondMax = -Infinity;
+
+  for (let num of arr) {
+    if (num < firstMax && num > secondMax) {
+      secondMax = num;
     }
+  }
 
-    let firstMax = -Infinity;
-    let secondMax = -Infinity;
-
-    for (let num of arr) {
-        if (num > firstMax) {
-            secondMax = firstMax;
-            firstMax = num;
-        } else if (num > secondMax && num < firstMax) {
-            secondMax = num;
-        }
-    }
-
-    return secondMax;
+  return secondMax;
 }
+
+// Udaaharan
+console.log(secondHighest([5, 1, 2, 3, 4])); // Output: 4
+console.log(secondHighest([-1, -2, -3, -4, -5])); // Output: -2
+console.log(secondHighest([])); // Output: -Infinity
+console.log(secondHighest([1])); // Output: -Infinity
+console.log(secondHighest([1, 1, 1, 1, 1])); // Output: -Infinity
 
